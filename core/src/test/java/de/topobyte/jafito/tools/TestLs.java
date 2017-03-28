f0fc29ad2da2d0a692043b0f6a336e272d49f41d
Sebastian Kürten <sebastian@topobyte.de>
2017-03-28 09:28:04 +0200

    Test for ls

---
+++ core/src/test/java/de/topobyte/jafito/tools/TestLs.java
@@ -0,0 +1,39 @@
+// Copyright 2017 Sebastian Kuerten
+//
+// This file is part of jafito.
+//
+// jafito is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// jafito is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with jafito. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.jafito.tools;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.topobyte.system.utils.SystemPaths;
+
+public class TestLs
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		List<Path> paths = new ArrayList<>();
+		paths.add(SystemPaths.CWD);
+
+		Ls ls = new Ls();
+		ls.list(paths);
+	}
+
+}
